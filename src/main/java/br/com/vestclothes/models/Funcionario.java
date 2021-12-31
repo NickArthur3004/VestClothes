8521b50b923c4bb91034b353180c0c444e7687ee
nicolas <nicolas.arthur2786@gmail.com>
2021-12-31 13:25:39 -0300

    criando entidade funcionario

--- src/main/java/br/com/vestclothes/models/Funcionario.java
+++ src/main/java/br/com/vestclothes/models/Funcionario.java
@@ -1,6 +1,5 @@
 package br.com.vestclothes.models;
 
-import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -11,18 +10,18 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "TB_Cliente")
-public class Cliente implements Serializable{
+@Table(name = "TB_Funcionario")
+public class Funcionario { 
 	
-	
-	private static final long serialVersionUID = 1L;
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(name = "nome")
 	private String name;
+
+	@Column(name = "email")
+	private String email;
 	
 	@Column(name = "dataNasc")
 	private Date dataNasc;
@@ -36,9 +35,6 @@ public class Cliente implements Serializable{
 	@Column(name = "cep")
 	private String cep;
 	
-	@Column(name = "numero")
-	private String numero;
-	
 	@Column(name = "logradouro")
 	private String logradouro;
 	
@@ -48,37 +44,44 @@ public class Cliente implements Serializable{
 	@Column(name = "cidade")
 	private String cidade;
 	
-	@Column(name = "complemento")
-	private String complemento;
+	@Column(name = "uf")
+	private String uf;
+	
+	@Column(name = "numResidencia")
+	private Integer numResidencia;
 	
 	@Column(name = "telefone")
 	private String telefone;
 	
-	@Column(name = "usuario_id")
+	@Column(name = "salario")
+	private Double salario;
+	
+	@Column(name = "id_usuario")
 	private Usuario usuario;
-	
-	
-	public Cliente() {
+	///////////////////////////////////////////////////////////////////
+	public Funcionario() {
 		super();
 	}
 
-	public Cliente(Long id, String name, Date dataNasc, String rg, String cpf, String cep, String numero,
-			String logradouro, String bairro, String cidade, String complemento, String telefone) {
+	public Funcionario(Long id, String name, String email, Date dataNasc, String rg, String cpf, String cep, String logradouro,
+			String bairro, String cidade, String uf, Integer numResidencia, String telefone, Double salario) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.email = email;
 		this.dataNasc = dataNasc;
 		this.rg = rg;
 		this.cpf = cpf;
 		this.cep = cep;
-		this.numero = numero;
 		this.logradouro = logradouro;
 		this.bairro = bairro;
 		this.cidade = cidade;
-		this.complemento = complemento;
+		this.uf = uf;
+		this.numResidencia = numResidencia;
 		this.telefone = telefone;
+		this.salario = salario;
 	}
-
+////////////////////////////////////////////////////////////////////////
 	public Long getId() {
 		return id;
 	}
@@ -127,14 +130,6 @@ public class Cliente implements Serializable{
 		this.cep = cep;
 	}
 
-	public String getNumero() {
-		return numero;
-	}
-
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-
 	public String getLogradouro() {
 		return logradouro;
 	}
@@ -159,12 +154,20 @@ public class Cliente implements Serializable{
 		this.cidade = cidade;
 	}
 
-	public String getComplemento() {
-		return complemento;
+	public String getUf() {
+		return uf;
 	}
 
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getTelefone() {
@@ -173,6 +176,22 @@ public class Cliente implements Serializable{
 
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
+	}
+
+	public Integer getNumResidencia() {
+		return numResidencia;
+	}
+
+	public void setNumResidencia(Integer numResidencia) {
+		this.numResidencia = numResidencia;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
 	}
 
 	public Usuario getUsuario() {
@@ -199,7 +218,7 @@ public class Cliente implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Cliente other = (Cliente) obj;
+		Funcionario other = (Funcionario) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -209,7 +228,5 @@ public class Cliente implements Serializable{
 	}
 	
 	
-	
-	
-	
+
 }
